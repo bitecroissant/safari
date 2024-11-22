38fb13ef26e6cd06a5bc9c042463ed2e1d54e12f
bitecroissant <bitecroissant@hotmail.com>
2024-11-22 14:32:24 +0800

    提交部分内容

---
+++ tmp.sql
@@ -0,0 +1,355 @@
+https://www.ruanyifeng.com/blog/2018/04/weekly-issue-1.html
+
+2、jsonstore.io
+https://github.com/bluzi/jsonstore
+通过 HTTP Header 读写 JSON 数据的免费 datastore。
+
+一线开发程序员总结出的开发经验，by 康亮
+https://cloud.tencent.com/developer/article/1004735
+
+Chrome DevTools 的一些使用技巧
+https://flaviocopes.com/chrome-devtools-tips/#drag-and-drop-in-the-elements-panel
+
+[文章] Programming Blockchains Step-by-Step, by Gerald Bauer
+https://github.com/openblockchains/programming-blockchains
+介绍如何使用 Ruby 语言从零开始写一个区块链实现，代码非常好懂，并有各种基础概念的解释。
+
+7、[电子书] Google 面试自学手册
+https://github.com/jwasham/coding-interview-university/blob/main/translations/README-cn.md
+一份爱好者整理的 Google 面试准备指南。
+
+3、flamebearer
+https://github.com/mapbox/flamebearer
+Node 应用的火焰图生成工具，用于性能分析。
+
+4、DNS Performance Test
+https://github.com/cleanbrowsing/dnsperftest
+DNS 响应时间的命令行比较脚本。
+
+5、merge-images
+https://github.com/lukechilds/merge-images
+多张图片合成一张图片的浏览器 JS 库，使用了 Canvas。
+
+6、Tabler
+https://github.com/tabler/tabler
+一个基于 Bootstrap4 的面板（dashboard）组件库。
+
+7、Etherpad
+https://etherpad.org/
+老牌的多人实时编辑协同工具。
+
+
+https://www.ruanyifeng.com/blog/2018/04/weekly-issue-2.html
+
+3、Jelly Mario Bros
+https://jellymar.io/
+水母版的《超级马里奥》网页游戏，所有东西都会像水母一样升缩。
+
+1、[电子书] A Gentle Introduction To Rust
+https://stevedonovan.github.io/rust-gentle-intro/readme.html
+Rust 语言入门教程
+
+2、[电子书] D3 Graph Theory
+https://d3gt.com/index.html
+图理论（graph theory）是重要的数学分支，在数据处理领域有着重要应用。这个教程采用可视化库 D3，把图理论变成了可视化互动教程。
+
+5、[文章] Probability Theory (For Scientists and Engineers)
+https://betanalpha.github.io/assets/case_studies/probability_theory.html
+一个概率论的概览性介绍，每个章节后面有一个 R 语言的小例子。
+
+6、[电子书] D3 In Depth
+https://www.d3indepth.com/
+可视化引擎 D3 的教程
+
+7、[文章] 分布式系统的几个基本概念
+https://blog.pragmaticengineer.com/distributed-architecture-concepts-i-have-learned-while-building-payments-systems/
+Uber 架构师分享在搭建分布式支付系统过程中，遇到的最重要的几个概念：SLA、scaling、Consistency、Durability、Idempotency等。
+
+8、[电子书] 解谜计算机科学
+https://www.yinwang.org/blog-cn/2018/04/13/csbook-chapter1
+王垠正在写的新书，目前只公布了第一章
+
+1、Mustard UI
+https://mustard-ui.com/
+一个简洁、好看的 CSS 框架，压缩后只有5.28KB。
+
+3、Remote Browser
+https://github.com/intoli/remote-browser
+一个浏览器自动化框架，可以用脚本控制已经打开的浏览器。
+
+4、rebound
+https://github.com/shobrook/rebound
+有人终于把这个工具写出来了，一旦 Python 或 JS 脚本报错，就到 Stack Overflow 取回报错信息的解释。
+
+5、React Slate
+http://react-slate.surge.sh/
+一个使用 React 组件写命令行脚本的框架。
+
+6、RSUITE
+https://rsuitejs.com/
+一个国产的 React 组件库。
+
+https://www.ruanyifeng.com/blog/2018/05/weekly-issue-3.html
+
+1、[文章] Meet the New Dialog Element
+https://keithjgrant.com/posts/2018/01/meet-the-new-dialog-element/
+HTML 5.2 新增了 < dialog> 元素，这个元素将来一定会广泛使用，因为它提供了浏览器原生 Modal 窗口。
+
+3、[文章] Active Record 与 Data Mapper 的差异
+https://culttt.com/2014/06/18/whats-difference-active-record-data-mapper/
+关系型数据库的操作通常采用 ORM 库，将表格转换成对象。ORM 主要分成两种类型：Active Record 与 Data Mapper。本文讨论这两种模型的差异和适用场景。
+
+4、[电子书]《计算与推断：数据科学基础》
+https://inferentialthinking.com/
+本书是加州大学伯克利分校《数据科学导论》课程的教材，现在开源了。
+
+6、[文章] Optimizing React: Virtual DOM explained, by Evil Martians
+https://evilmartians.com/chronicles/optimizing-react-virtual-dom-explained
+React 的虚拟 DOM 机制的详细解释， JSX 代码如何变成虚拟 DOM 的数据结构。
+
+7、[教程] Canvas: Draw on the web
+https://www.yuque.com/airing/canvas/readme
+国产的 HTML5 Canvas 教程。
+
+5、Algolia 的内部实现
+https://stackshare.io/posts/how-algolia-built-their-realtime-search-as-a-service-product
+Algolia 是目前很红的搜索服务提供商。它最初的想法是提供手机应用离线搜索的 SDK。但是，不久就有客户咨询，能不能托管网站的搜索服务。Algolia 意识到，这可能是他们转型的机会。他们现在可以搜索 Pebble，WeFunder，CodeCombat 和 HackerNews 一系列网站。我们与他们坐下来，了解他们的搜索产品及其背后的技术，访问分为两部分：
+- Algolia 是如何开始的，以及它与众不同的原因
+- Algolia 背后的技术
+
+地球上最长的不经过海洋的直线
+https://www.thatsmags.com/china/post/10356/the-longest-straight-path-on-earth-starts-in-east-china-ends-in-liberia
+从浙江省台州市南部的石塘镇开始向西走，然后是武汉、西安、青海。穿越喜马拉雅山脉后，你会到达塔吉克斯坦，通过乌兹别克斯坦，土库曼斯坦，伊朗，伊拉克，约旦，以色列，埃及（穿过开罗市中心！）、利比亚，尼日尔，布基纳法索，加纳，科特迪瓦，最后到达利比里亚。
+由于地图投影，这条线看起来是弯的，但是它实际上是直线。
+
+1、[文章] Node.js 最佳托管商
+https://railsware.com/blog/best-hosting-for-node-js-app/
+现在，各大云服务商都支持 Node.js 主机，本文对比了最大的 6 家服务商各自的优缺点。
+
+3、[电子书] 《React in patterns》
+https://github.com/krasimir/react-in-patterns
+React 如何使用各种编程模式？
+
+5、css-protips
+https://github.com/AllThingsSmitty/css-protips/tree/master/translations/zh-CN
+一个收集 CSS 使用技巧的库。
+
+6、github-contributions-chart（Demo）
+https://github.com/sallar/github-contributions-chart
+GitHub 的用户主页有一张图，每一天就是一个小格子。你只要在这一天提交了代码，这个格子就是绿色。但是，这张图只显示过去的一年，即365个格子。
+这个工具可以生成从你加入 GitHub 那天起的所有格子。
+7、bat
+https://github.com/sharkdp/bat
+命令行的 cat 命令用来显示文件的内容，bat 命令完全跟 cat 一致，只有一个地方不一样，就是现在的内容会带有行号和代码高亮。
+
+3、SQL 命名约定
+https://launchbylunch.com/posts/2014/Feb/16/sql-naming-conventions/
+数据库的表和字段怎么起名？这也是一门学问。下面是数据库的一些命名原则。
+
+1、Douglas Crockford 的名片
+https://seriot.ch/projects/parsing_json.html#1
+2001年，Douglas Crockford 发明了 JSON 格式。他声称这种格式极其简单，全部语法可以印在一张名片上，而且所有应该有的语法都有了，以后也不需要再增订，因此 JSON 格式没有版本号。上面就是他的名片的背面图案。
+但是，我们现在知道，JSON 格式的解析器很难写，因为它没有规定如何处理各种边界情况。
+
+
+https://www.ruanyifeng.com/blog/2018/05/weekly-issue-5.html
+
+1、［文章］最简单的马尔可夫链图解（英文）
+https://setosa.io/ev/markov-chains/
+上面是最简单的马尔可夫链图解。系统包括 A 和 B 两个状态。一共有四种转化路径：A 到 A、A 到 B、B 到 A，B 到 B。每种路径的可能性都是 50%，就得到了随机的运动轨迹。
+一个通俗的实例是，每天有"晴天"和"下雨"两种天气，天气变化的概率是50%，那么马尔可夫链就可以生成一个未来一周天气的模型。
+
+2、[文章] 决策树模型入门（英文）
+https://fritz.ai/introduction-to-decision-tree-learning/
+决策树模型（Decision Tree）是最简单的机器学习模型，也最容易理解。当决策受到多个因素影响时，这个模型相当有用。
+
+4、[代码] Elm + Rust 开发桌面应用（英文）
+https://github.com/huytd/kanban-app
+目前，使用 Web 技术开发桌面应用，主要通过 Electron。它的缺点是，有时你只是想要在桌面上展示一个网页，不需要跟本地文件系统交互，但是不得不把整个 Chromium 浏览器和 V8 引擎包含在这个应用里面，导致不管逻辑是否复杂，任何一个 Electron 应用都至少有几十MB的大小。
+这个项目展示了另一种开发桌面应用的可能。它的原理是，任何操作系统都有自己的 WebView，也就是说可以在应用程序里面调用 WebView 展示网页。那么可以使用 Rust 语言打包 WebView，而 JS 脚本部分交给 Elm 语言生成。由于 WebView 是系统提供的，所以打包出来非常小，一般只有几百KB，资源占用也很少。
+
+5、[文章] 高可用的部署技巧（英文）
+https://medium.com/@NetflixTechBlog/tips-for-high-availability-be0472f2599c
+Netflix 公司的工程师介绍，他们如何部署代码，提高服务的可用性，比如使用红黑部署、设置部署窗口等等。
+
+6、[教程] 哈佛大学《CS109：数据科学基础》的课程资料（英文）
+http://cs109.org/
+1xx 编号的课程都是本科生的基础课程，我看了一下，讲的是 Python、基础的统计学知识和数学模型，提供免费的课程视频和PPT 下载。
+
+7、[笔记]《技术面试需要掌握的基础知识整理》（中文）
+https://github.com/CyC2018/CS-Notes
+技术面试需要掌握的基础知识整理。
+
+8、[资料] 互联网公司技术架构（中文）
+https://github.com/davideuler/architecture.of.internet-product
+作者收集的国内各大互联网公司技术架构的资料
+
+1、sonarwhal
+https://sonarwhal.com/
+微软推出的开源网页 lint 工具（源码仓库），可以在线扫描网页，提供无障碍、互通性、性能、PWA、安全五个方面的扫描结果，给出改进点。
+
+2、IPBlade
+https://chromewebstore.google.com/detail/ipblade-privacy-protector/hojjgiebfmgmlpjojjkaehgohkolmoea?pli=1
+有的网络服务限制 IP 地址，比如只有中国的 IP 地址才能使用。这个 Chrome 插件点击一下就可以改变浏览器的 IP 地址，从而绕过这些限制。免费版只能改成美国的 IP 地址。
+
+3、.gitignore 文件的命令行生成工具
+https://github.com/TejasQ/add-gitignore
+.gitignore 文件设置了哪些文件可以不用进入 Git 版本管理。这个命令行工具可以根据项目类型，自动生成 .gitignore 文件。
+
+5、love2.io
+https://love2.io/
+一个收集开源书籍的网站。
+
+
+https://www.ruanyifeng.com/blog/2018/05/weekly-issue-6.html
+
+最近，我有一个行程，可能要去日本。我还没去过日本呢，听说日本人普遍听不懂英语，我又不会说日语，这可怎么办？
+突然想到，"谷歌翻译"这个 APP 也许能解决语言问题。它有一个"对话实时翻译"功能，可以同时监听两种语言，听到中文就自动说出日语，听到日语就说出中文。我试了一下，翻译效果之好令人震惊，完全是真人发音，翻译非常准确。建议大家也装一个玩玩，亲身体验自己说出的话变成流利的日语，肯定能震撼到你。
+两个人同时对着手机说话，还是比较尴尬的，因此谷歌还推出了 Buds 蓝牙耳机。你说出的话通过耳机的话筒传入手机，让手机播放翻译好的版本给对方听。对方的回应被翻译以后，再通过耳机传给你。这样的话，对话可以始终是面对面。
+看着这个玩意，我心想将来还需要苦学外语吗？很多人学了十年，口语还是结结巴巴，词不达意。照我说，那就别学了，大好青春干什么不好，何必用来背单词，反正以后人工智能可以帮你说外语。
+
+4、isomorphic-git
+https://github.com/isomorphic-git/isomorphic-git
+Git 的 JavaScript 实现，这意味着你可以在 JS 里进行 Git 操作，比如从抓取/提交 commit，进行 diff 或 merge 等等。
+
+
+https://www.ruanyifeng.com/blog/2018/06/weekly-issue-7.html
+
+1、[网站] DiscoverDev.io（英文）
+https://www.discoverdev.io/
+该网站每天列出5～10篇当天值得阅读的软件文章。
+
+3、[文章] 气压的知识（英文）
+https://www.thoughtco.com/low-and-high-pressure-1434434
+气压就是大气的压力，一个标准大气压等于760毫米汞柱。有的地方气压高，有的地方气压低，这是风形成的根本原因，台风就是巨大的热带低气压。地球上，赤道由于高温蒸发是低压区，极地由于冷空气下降是高压区，这就形成了地球的风带。
+
+4、[教程] Flask 教程的中文版（中文）
+https://github.com/luhuisicnu/The-Flask-Mega-Tutorial-zh
+Flask 是 Python 的 Web 开发框架。这个教程教你如何用 Python 和 Flask 来创建 Web 应用。
+
+5、[资料] arl
+https://github.com/kaxap/arl
+各种语言最受欢迎的项目，按照 GitHub Star 的数量排行。
+
+6、[资料] 谷歌官方的 Bash 脚本风格要求（英文）
+https://google.github.io/styleguide/shellguide.html
+Bash 脚本的语法远比编程语言宽松，你很容易写出难以阅读、风格混乱的脚本，可以参考谷歌如何写 Bash 脚本。
+
+2、ShellCheck
+https://www.shellcheck.net/
+Bash 脚本的开源语法检查器。
+
+3、Crunch
+https://github.com/chrissimpkins/Crunch
+一个效果非常好的 PNG 图片压缩工具。
+
+6、Material-UI
+https://mui.com/x/react-date-pickers/
+大家知道，谷歌的材质化组件（Material Design）是 Angular 专用的。社区维护的 React 移植版Material-UI，今天发布 1.0 版，已经完全可用了。以后，React 项目照样可以有官方安卓 UI 的那种材质化效果。
+
+1、Dribble 的按钮
+https://www.toptal.com/designers/ui/button-design-dribbble-timeline
+有人回顾了过去八年，设计网站 Dribbble 主页按钮的变化。图一是2009年的按钮，图二是2017年的按钮。八年的图片放在一起，可以看到设计的趋势变得越来越简单和平面化。
+
+
+https://www.ruanyifeng.com/blog/2018/06/weekly-issue-8.html
+
+5、[PDF] Awk 编程语言（英文）
+https://ia803404.us.archive.org/0/items/pdfy-MgN0H1joIoDVoIC7/The_AWK_Programming_Language.pdf
+awk 是 Unix 命令行处理文本的有用工具，这里是1988年出版的 awk 教程。Unix 命令行高度稳定，所以这本教材也没那么过时。
+
+1、CalVer
+https://calver.org/
+语义版本（SemVer）现在很流行，就是根据 API 的变动为版本编号。这个网址提出另一种编号方法：根据日历编号，版本号就是发布日期。很多软件已经在用了，最有名的就是 Ubuntu，它的版本格式是 yy.mm，比如 16.04。
+
+8、Cool Backgrounds
+https://coolbackgrounds.io/
+自动生成背景图片的工具网站，现在提供五种风格，每种都可以定制，看上去赏心悦目。
+
+https://www.ruanyifeng.com/blog/2018/06/weekly-issue-9.html
+
+2、[游戏] while True: learn()
+https://luden.io/wtl/
+一个帮助玩家学习人工智能的游戏。你扮演一个人工智能专家，在游戏的引导下解决各种问题。
+
+4、[视频] 计算机科学速成课（英文中字）
+https://github.com/1c7/crash-course-computer-science-chinese
+志愿者从 Youtube 搬到 B 站的40集视频教程。
+
+5、[仓库] awesome-ruby-china（中文）
+https://github.com/liukun-lk/awesome-ruby-china
+Ruby China 论坛的精华贴整理。
+
+1、Drawpile
+https://drawpile.net/about/
+多人实时协同作画的桌面应用。
+
+2、wired-elements
+https://github.com/rough-stuff/wired-elements
+一个有手绘效果的网页组件库。但是，真正特别之处在于它的底层是 Web components，让我们看到了除了React/Vue之外，还有其他的路。
+
+6、抖音机器人
+https://github.com/wangshub/Douyin-Bot
+作者用 Python + ADB 做的 Bot。它会自动打开 APP 对视频截图，然后请求腾讯的 人脸识别 API，当颜值大于门限值 BEAUTY_THRESHOLD时，点赞并关注，接着翻到下一页，重复进行该过程。
+
+3、black
+https://github.com/psf/black
+Python 语言的格式要求特别高，因为它通过缩进判断语法区块。现在有了这个工具，就可以自动化格式化 Python 代码，所以你不用担心写出风格一团糟的代码。
+
+
+https://www.ruanyifeng.com/blog/2018/06/weekly-issue-10.html
+
+6、GraphQL 指南
+https://blog.graphql.guide/introducing-the-graphql-guide-11a5ae48628a
+jQuery 的作者 John Resig 出新书《GraphQL 指南》了。他的时间现在都投在 React 开发。
+
+1、Reach router
+https://reach.tech/router/
+React router 的两位创始人闹翻了，其中一位另起门户，写了一个功能相同的 Reach router
+
+2、Lottie
+https://airbnb.io/lottie/
+Airbnb 推出的动画效果库，可以把 Adobe After Effects 制作的动画用于 Web、安卓和 iOS。
+
+4、wtf
+https://github.com/wtfutil/wtf
+在终端窗口显示控制台面版。
+
+6、hiper
+https://github.com/pod4g/hiper
+一个查看网页加载性能的命令行程序。
+
+7、Gource
+https://gource.io/
+Gource 是一个很好玩的可视化工具，可以将代码仓库的历史变成视频，支持 Git 和 SVN 等多种格式。只要在仓库目录执行 gource 命令，就能看到提交历史的视频。
+
+2、Radio Garden
+https://radio.garden/
+世界地图上每一个绿点，就代表一个当地的电台。光标移上去，就可以听到该电台的现场直播。
+
+
+https://www.ruanyifeng.com/blog/2018/06/weekly-issue-11.html
+
+8、Python 的数据类型： list 与 tuple 的区别（英文）
+https://rushter.com/blog/python-lists-and-tuples/
+Python 语言之中，元组（tuple）和列表（list）都表示数据的序列。最大的区别是 tuple 是不可变的，即不能新增、删除、修改 tuple。
+
+9、纯 CSS 效果展示
+转动的电风扇
+https://codepen.io/zschaffter/pen/GdWQeV/
+动态火焰效果
+https://codepen.io/jkantner/pen/gKRKKb
+
+4、Java 面试知识点
+https://github.com/crossoverJie/JCSprout
+这个库收集各种 Java 面试的知识点。
+
+2、neofetch
+https://github.com/dylanaraps/neofetch·
+一个显示 Linux 服务器信息的命令行工具。
+
+
+
+
+
+
