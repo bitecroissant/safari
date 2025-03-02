e8c85a5b54c6f33473414b09f8eeb700fde2eba5
bitecroissant <bitecroissant@hotmail.com>
2025-03-02 16:13:03 +0800

    整理一下 sql

---
+++ dbmigration/migration_250302.sql
@@ -0,0 +1,2 @@
+INSERT INTO EventDates (isDeleted, gmtCreate, gmtModified, `group`, eventName, happenAt, datesStatus) VALUES 
+(0, '2024-12-07 14:59:08 032', '2024-12-07 14:59:08 032', '田', '莫后演唱会', '2026-12-07', 'active')
